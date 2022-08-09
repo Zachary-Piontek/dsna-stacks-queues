ec61f27f9bb1be1263bdab35412884288a18d48f
Zachary Piontek <fortunefavorsthebrave@protonmail.com>
2022-08-09 10:39:16 -0700

    stack started

--- index.js
+++ index.js
@@ -1,4 +1,26 @@
-class Stack {}
+class Stack {
+    #list = [];
+
+    constructor(initList) {
+        if (initList) this.#list = initList;
+    }
+     addBack(item) {
+        this.#list.push(item)       
+    }
+
+    addFront(item) {
+        this.#list.pop();
+    }
+
+    addPeek(item) {
+        this.#list.peek();
+    }
+}
+
+const myStructure = new MyData (['joe', 'mary', 'mike']);
+
+myStructure.addFront('magic');
+
 
 class Queue {}
 
